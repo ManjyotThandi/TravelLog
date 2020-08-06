4469597ac0a74eb574ba46e5731f2530afc787f5
Manjyot Thandi <manjyot.thandi@gmail.com>
2020-08-06 15:28:00 -0400

    Added in popups for each location

--- client/src/App.js
+++ client/src/App.js
@@ -11,8 +11,9 @@ function App() {
     height: '100vh',
     latitude: 37.7577,
     longitude: -122.4376,
-    zoom: 8
+    zoom: 2
   });
+  const [showPopup, setPopup] = useState({});
 
   // equiv of componentdidmount
   useEffect(() => {
@@ -32,26 +33,48 @@ function App() {
     >
       {
         logEntries.map(entry => (
-          <Marker key={entry._id} latitude={entry.latitude} longitude={entry.longitude}>
-            <svg
-              className="marker"
-              viewBox="0 0 24 24"
-              strokeWidth="2"
-              fill="none"
-              strokeLinecap="round"
-              strokeLinejoin="round" >
-              <path d="M21 10c0 7-9 13-9 13s-9-6-9-13a9 9 0 0 1 18 0z"></path><circle cx="12" cy="10" r="3"></circle>
-            </svg>
-          </Marker>
+          <>
+            <Marker key={entry._id} latitude={entry.latitude} longitude={entry.longitude}>
+              <div onClick={() => {
+                setPopup({
+                  ...showPopup,
+                  [entry._id]: true
+                })
+              }}>
+                <svg
+                  className="marker"
+                  viewBox="0 0 24 24"
+                  strokeWidth="2"
+                  fill="none"
+                  strokeLinecap="round"
+                  strokeLinejoin="round" >
+                  <path d="M21 10c0 7-9 13-9 13s-9-6-9-13a9 9 0 0 1 18 0z"></path><circle cx="12" cy="10" r="3"></circle>
+                </svg>
+              </div>
+            </Marker>
+            {
+              showPopup[entry._id] && (
+                <Popup
+                  longitude={entry.longitude}
+                  latitude={entry.latitude}
+                  closeButton={true}
+                  closeOnClick = {true}
+                  onClose={() => {
+                    setPopup({
+                      ...showPopup,
+                      [entry._id]: false
+                    })
+                  }}>
+                  <div className = "popup">
+                    <h3>{entry.title}</h3>
+                    <p>{entry.comments}</p>
+                  </div>
+                </Popup>
+              )
+            }
+          </>
         ))
       }
-        <Popup 
-        longitude={0} 
-        latitude={0} 
-        closeButton={false} 
-        closeOnClick={false}>
-           <div>Hi There</div>
-        </Popup>
     </ReactMapGL>
   );
 }
